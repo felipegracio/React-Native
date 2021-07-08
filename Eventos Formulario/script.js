12d55772e5917910878a3f77234b325c374aaa0c
felipegracio <felipegracio@gmail.com>
2021-07-08 16:05:57 -0300

    primeiros passos

---
+++ Eventos Formulario/script.js
@@ -0,0 +1,17 @@
+function focado(){
+
+	console.log("Destacar Campo");
+}
+
+
+
+function desfocar(){
+
+	console.log("desfocado");
+}
+
+function selecionada(objeto){
+
+	console.log("A cidade selecionada foi: "+ objeto.value);
+
+}
