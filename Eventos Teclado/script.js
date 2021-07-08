a78d8f2a3eb1be95d606abc71c251214d4346a37
felipegracio <felipegracio@gmail.com>
2021-07-08 15:18:47 -0300

    primeiros passos

---
+++ Eventos Teclado/script.js
@@ -0,0 +1,15 @@
+
+
+function tecladoapertado(event){
+
+	console.log("teclado apertado " + event.keyCode);
+
+
+	if(event.keyCode ==13){
+
+		alert("Você apertou o ENTER");
+
+	}
+
+}
+
---
+++ Eventos de página/script.js
@@ -0,0 +1,6 @@
+
+function carregou(){
+
+	alert("pagina carregada!");
+
+}
